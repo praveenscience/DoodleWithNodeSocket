681d98e9aa73815ee6761c50ebb669307fc31a46
Praveen Kumar <praveenscience@gmail.com>
2020-11-28 11:12:43 +0000

    Created a Public Static page with Express.

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.json("Hello Everyone!");
-});
+app.use("/", express.static(__dirname + "/public"));
 
 app.listen(port, () => {
   console.log("Server started in port " + port + ".");
