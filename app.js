7ec81bf4ed82298cab00eb021ffaf66e1dd5f3b6
Praveen Kumar <praveenscience@gmail.com>
2020-11-28 11:07:01 +0000

    Added a GET Request.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.json("Hello Everyone!");
+});
+
 app.listen(port, () => {
   console.log("Server started in port " + port + ".");
 });
