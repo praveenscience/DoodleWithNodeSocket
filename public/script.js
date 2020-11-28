503fde69adf93d023e33a2b7f17e37d8b3285c2e
Praveen Kumar <praveenscience@gmail.com>
2020-11-28 11:44:54 +0000

    Event listener functions added for three cases.

--- public/script.js
+++ public/script.js
@@ -8,3 +8,50 @@ var drawing = false;
 var current = {
   color: "black"
 };
+
+function drawLine(x0, y0, x1, y1, color, emit) {
+  context.beginPath();
+  context.moveTo(x0, y0);
+  context.lineTo(x1, y1);
+  context.strokeStyle = color;
+  context.lineWidth = 2;
+  context.stroke();
+  context.closePath();
+}
+
+function mousedown(e) {
+  drawing = true;
+  current.x = e.clientX || e.touches[0].clientX;
+  current.y = e.clientY || e.touches[0].clientY;
+}
+
+function mouseup(e) {
+  if (!drawing) {
+    return;
+  }
+  drawing = false;
+  drawLine(
+    current.x,
+    current.y,
+    e.clientX || e.touches[0].clientX,
+    e.clientY || e.touches[0].clientY,
+    current.color,
+    true
+  );
+}
+
+function mousemove(e) {
+  if (!drawing) {
+    return;
+  }
+  drawLine(
+    current.x,
+    current.y,
+    e.clientX || e.touches[0].clientX,
+    e.clientY || e.touches[0].clientY,
+    current.color,
+    true
+  );
+  current.x = e.clientX || e.touches[0].clientX;
+  current.y = e.clientY || e.touches[0].clientY;
+}
