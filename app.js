85cecffa5a8900560a9d01383192416b288e49ab
Praveen Kumar <praveenscience@gmail.com>
2020-11-28 11:21:42 +0000

    Included HTTP Server & connected with IO.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = 3000;
 
 app.use("/", express.static(__dirname + "/public"));
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log("Server started in port " + port + ".");
 });
