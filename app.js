193f49b48a2cb83478eadf78e2f4d6e8918bee17
Praveen Kumar <praveenscience@gmail.com>
2020-11-28 11:23:43 +0000

    Created a handler for broadcasting data.

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const port = 3000;
 
 app.use("/", express.static(__dirname + "/public"));
 
+const onConnection = socket => {
+  socket.on("drawing", data => socket.broadcast.emit("drawing", data));
+};
+
+io.on("connection", onConnection);
+
 http.listen(port, () => {
   console.log("Server started in port " + port + ".");
 });
